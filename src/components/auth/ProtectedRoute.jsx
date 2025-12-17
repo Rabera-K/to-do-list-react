2277034e3d38d683781a7529749b2dc30662a08c
Rabera <raberakaunda@gmail.com>
2025-12-17 17:50:02 +0300

    ProtectedRoute authentication added

---
+++ src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import {Navigate} from 'react-router-dom';
+import{useAuthContext} from '../../context/AuthContext';
+
+function ProtectedRoute({children}){
+    const{user, loading}=useAuthContext();
+
+    if(loading) {
+        return <div>Loading...</div>
+    }
+
+    if(!user){
+        return <Navigate to='/login'/>
+    }
+     return children;
+}
+
+export default ProtectedRoute;
