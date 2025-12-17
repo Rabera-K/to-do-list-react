e1010d59002cfacf0991afaec1fbcc23aef1d5b2
Rabera <raberakaunda@gmail.com>
2025-12-17 17:43:58 +0300

    added authContext and updated app.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,2 +1,31 @@
 import {createContext, useState, useContext, useEffect} from 'react';
-import ApiService
+import useAuth from '../hooks/useAuth';
+
+
+const AuthContext= createContext();
+
+export function useAuthContext(){
+    const context=useContext(AuthContext);
+    if(!context){
+        throw new Error('useAuthContext must be used within AuthProvider')
+    }
+    return context;
+}
+
+export function AuthProvider({children}){
+    const auth=useAuth();
+
+    const contextValue={
+        user: auth.user,
+        loading: auth.loading,
+        error: auth.error,
+        login: auth.login,
+        signup: auth.signup,
+        logout: auth.logout,
+        isAuthenticated: !!auth.user,
+    };
+
+    return(
+        <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+    );
+}
